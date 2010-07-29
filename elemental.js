71b0e11f664cf6d44bf81214bf9b8c421f0dd0c0
Dmitry Baranovskiy <Dmitry@Baranovskiy.com>
2010-07-29 17:22:57 +1000

    0.2 Added ability to use it as a function, i.e. elemental()("tags") + eve support

--- elemental.js
+++ elemental.js
@@ -1,19 +1,29 @@
+/*!
+ * Elemental 0.2 - Simple JavaScript Tag Parser
+ *
+ * Copyright (c) 2010 Dmitry Baranovskiy (http://dmitry.baranovskiy.com/eve/)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+ 
 (function () {
     function parse(s) {
         s = s || Object(s);
-        var pos = this._pos,
-            l = this._l,
-            len = s.length;
-        for (;pos < l + len; pos++) {
-            step.call(this, at(s, pos), at(s, pos + 1), at(s, pos - 1));
+        var pos = 1,
+            len = s.length + 1,
+            p, c, n = at(s, 0);
+        for (;pos < len; pos++) {
+            p = c;
+            c = n;
+            n = at(s, pos);
+            this.raw += c;
+            step.call(this, c, n, p);
         }
-        this._l += len;
     }
-    
+
     function at(s, i) {
-        return s.charAt ? s.charAt(i) : s[i];
+        return s && (s.charAt ? s.charAt(i) : s[i]);
     }
-    
+
     function on(name, f) {
         this.events = this.events || {};
         this.events[name] = this.events[name] || [];
@@ -21,17 +31,22 @@
     }
 
     function event(name, data, attr) {
+        if (typeof eve == "function") {
+            eve("elemental", name, data, attr || "", this.raw);
+            eve("elemental." + name, null, data, attr || "", this.raw);
+        }
         var a = this.events && this.events[name],
             i = a && a.length;
         while (i--) {
-            this.events[name][i](data, attr || "");
+            this.events[name][i](data, attr || "", this.raw);
         }
+        this.raw = "";
     }
-    
-    function clear() {
-        this._pos = 0;
-        this._l = 0;
+
+    function end() {
         this.mode = "text";
+        this.textchunk = "";
+        this.event("eof");
     }
 
     var whitespace = /[\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000]/;
@@ -41,10 +56,12 @@
             case "text":
                 switch (c) {
                     case "<":
-                        this.name = "";
+                        this.nodename = "";
                         this.attr = {};
                         this.mode = "tag name start";
+                        this.raw = this.raw.slice(0, -1);
                         this.textchunk && this.event("text", this.textchunk);
+                        this.raw += c;
                         this.textchunk = "";
                     break;
                     default:
@@ -133,63 +150,63 @@
                 if (!whitespace.test(c)) {
                     this.mode = "close tag name";
                     this.tagname = "";
-                    this.name = "";
+                    this.nodename = "";
                 } else {
                     break;
                 }
             case "close tag name":
                 if (whitespace.test(c)) {
-                    this.tagname = this.name;
+                    this.tagname = this.nodename;
                 } else switch (c) {
                     case ">":
-                        this.event("/tag", this.tagname || this.name);
+                        this.event("/tag", (this.tagname || this.nodename).toLowerCase());
                         this.mode = "text";
                     break;
                     default:
-                        !this.tagname && (this.name += c);
+                        !this.tagname && (this.nodename += c);
                     break;
                 }
             break;
             case "tag name":
                 if (whitespace.test(c)) {
-                    this.tagname = this.name;
-                    this.name = "";
+                    this.tagname = this.nodename;
+                    this.nodename = "";
                     this.mode = "attr start";
                 } else switch (c) {
                     case ">":
-                        this.event("tag", this.name);
+                        this.event("tag", this.nodename.toLowerCase());
                         this.mode = "text";
                     break;
                     default:
-                        this.name += c;
+                        this.nodename += c;
                     break;
                 }
             break;
             case "attr start":
                 if (!whitespace.test(c)) {
                     this.mode = "attr";
-                    this.name = "";
+                    this.nodename = "";
                 } else {
                     break;
                 }
             case "attr":
                 if (whitespace.test(c) || c == "=") {
-                    this.attr[this.name] = "";
+                    this.attr[this.nodename] = "";
                     this.mode = "attr value start";
                 } else switch (c) {
                     case ">":
-                        if (this.name == "/") {
+                        if (this.nodename == "/") {
                             delete this.attr["/"];
                             this.event("tag", this.tagname, this.attr);
                             this.event("/tag", this.tagname, true);
                         } else {
-                            this.name && (this.attr[this.name] = "");
+                            this.nodename && (this.attr[this.nodename] = "");
                             this.event("tag", this.tagname, this.attr);
                         }
                         this.mode = "text";
                     break;
                     default:
-                        this.name += c;
+                        this.nodename += c;
                     break;
                 }
             break;
@@ -218,7 +235,7 @@
                         }
                     break;
                     default:
-                        this.attr[this.name] += c;
+                        this.attr[this.nodename] += c;
                     break;
                 }
             break;
@@ -226,16 +243,19 @@
     }
 
     function elemental() {
-        return {
-            _pos: 0,
-            _l: 0,
-            mode: "text",
-            parse: parse,
-            on: on,
-            event: event,
-            clear: clear
+        var out = function (s) {
+            out.parse(s);
         };
+        out.mode = "text";
+        out.textchunk = "";
+        out.raw = "";
+        out.parse = parse;
+        out.on = on;
+        out.event = event;
+        out.end = end;
+        return out;
     }
+    
 
     (typeof exports == "undefined" ? this : exports).elemental = elemental;
 })();
